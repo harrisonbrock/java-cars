4160c49010f467c6e6d6112c9b03ecf2c9ce43b6
Harrison Brock <harrison@harrisonbrock.com>
2019-02-15 11:14:51 -0500

    create new project

---
+++ src/main/java/com/harrisonbrock/carsapi/CarsApiApplication.java
@@ -0,0 +1,14 @@
+package com.harrisonbrock.carsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsApiApplication.class, args);
+    }
+
+}
+
